cdf9544398992cd600d7eab2dfd3a41f2b879543
Iain Grant <iain.grant@sepa.org.uk>
2023-03-07 01:15:43 +0000

    Adding Get Robots to redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,23 @@
-import { CHANGE_SEARCH_FIELD } from "./constants"
+import { 
+    CHANGE_SEARCH_FIELD, 
+    REQUEST_ROBOTS_PENDING, 
+    REQUEST_ROBOTS_SUCCESS, 
+    REQUEST_ROBOTS_FAILED 
+} from "./constants"
+
 
 export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD,
     payload: text
 });
+
+export const requestRobots = () => async (dispatch) => {
+    dispatch({ type: REQUEST_ROBOTS_PENDING });
+    try {
+        const response = await fetch('https://jsonplaceholder.cypress.io/users');
+        const users = await response.json();
+        await dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: users });
+    } catch(error) {
+        await dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error });
+    } 
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { combineReducers } from 'redux';
+
+export const rootReducer = combineReducers({searchRobots, requestRobots});
+
 
 const logger = createLogger();
 export default configureStore({
-    middleware: [logger],
-    reducer: searchRobots
+    middleware: [thunkMiddleware, logger],
+    reducer: rootReducer
 });
